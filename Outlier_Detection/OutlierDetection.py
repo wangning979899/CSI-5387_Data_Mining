092e351055f180c6f6a8be1b58ee09ffcde18191
Paul <33930832+AaPaul@users.noreply.github.com>
2020-03-23 20:51:49 -0400

    Add files via upload

--- Outlier_Detection/OutlierDetection.py
+++ Outlier_Detection/OutlierDetection.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-
+from sklearn.preprocessing import MinMaxScaler
 f_name = '../Real estate valuation data set.xlsx'
 # Set K value
-k = 5
-threshold = 3
+k = 8
+threshold = 2
 iteration = 100
 data = pd.read_excel(f_name)
 
@@ -15,8 +15,10 @@ data = data.drop(columns='No')
 # Elbow method to determine the K value
 '''
 K_col = [ x+1 for x in range(15)]
-df = pd.read_excel(f_name)
-df = df.drop(columns='No')
+# df = pd.read_excel(f_name)
+# df = df.drop(columns='No')
+df = MinMaxScaler().fit_transform(data)
+df = pd.DataFrame(df, columns=data.columns)
 sse = []
 for k in K_col:
     model = KMeans(n_clusters=k).fit(df)
@@ -28,17 +30,29 @@ plt.ylabel('SSE')
 plt.title("Elbow Plot")
 plt.savefig("../Figures/Elbow Plot.png")
 '''
-model = KMeans(n_clusters=k, max_iter=iteration)
-model.fit(data)
 
-r = pd.concat([data, pd.Series(model.labels_, index=data.index)], axis=1)
-r.columns = list(data.columns) + ["Cluster"]
+# Standardize dataset
+data_std = MinMaxScaler().fit_transform(data)
+data_std = pd.DataFrame(data_std, columns=data.columns)
+
+model = KMeans(n_clusters=k, max_iter=iteration)
+# model.fit(data)
+model.fit(data_std)
+
+# r = pd.concat([data, pd.Series(model.labels_, index=data.index)], axis=1)
+# r.columns = list(data.columns) + ["Cluster"]
+r = pd.concat([data_std, pd.Series(model.labels_, index=data_std.index)], axis=1)
+r.columns = list(data_std.columns) + ["Cluster"]
+
+
 
 norm = []
 for i in range(k):
     norm_tmp = r[['X1 transaction date', 'X2 house age', 'X3 distance to the nearest MRT station',
-                     'X4 number of convenience stores', 'X5 latitude', 'X6 longitude',
-                     'Y house price of unit area']][r['Cluster'] == i] - model.cluster_centers_[i]
+                  'X4 number of convenience stores', 'X5 latitude', 'X6 longitude',
+                  'Y house price of unit area']][r['Cluster'] == i] - model.cluster_centers_[i]
+
+    # Use L2-Norm to calculate the absolute distance
     norm_tmp = norm_tmp.apply(np.linalg.norm, axis=1)
     norm.append(norm_tmp / norm_tmp.median())
 
@@ -47,10 +61,8 @@ norm = pd.concat(norm)
 plt.figure(figsize=(12, 8))
 plt.title('Outlier Detection')
 norm[norm <= threshold].plot(style='bo', legend=True, label='Normal Data')
-# df_norm = pd.DataFrame(norm[norm <= threshold], columns='Normal Data')
-# df_norm.plot(style='bo')
 discrete_points = norm[norm > threshold]
-# len(discrete_points) = 56
+# len(discrete_points) = 15
 discrete_points.plot(style='ro', legend='True', label='Outliers')
 # plt.legend(handles=[l1,l2],labels=['up','down'],loc='best')
 plt.xlabel('No')
